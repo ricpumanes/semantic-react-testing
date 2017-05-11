59253f33a9c1083f478a7eda253ec4baa651ba22
ric <ric@snapzio.com>
2017-05-11 14:15:43 +0800

    added active route

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
 
-import Songs from './Songs';
 import Header from './Header';
 
 class App extends Component {
 
   componentDidMount() {
-    
+
   }
 
   render() {
     return (
       <div>
-        <Header />
+        <Header {...this.props} />
         <Container id="mainContainer">
           {this.props.children}
         </Container>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,14 @@ import { Menu } from 'semantic-ui-react';
 
 export default class Header extends React.Component {
   render() {
+    const { location } = this.props;
     return (
       <div>
         <Menu pointing secondary>
-          <Menu.Item name='home'>
+          <Menu.Item name='home' active={location.pathname === "/"}>
             <Link to="/">Home</Link>
           </Menu.Item>
-          <Menu.Item name='songs'>
+          <Menu.Item name='songs' active={location.pathname === "/songs"}>
             <Link to="/songs">Songs</Link>
           </Menu.Item>
         </Menu>
